b01fa66bb0e36019f27097dcdf2a5cdf0a71e4ae
Alex Perez <apramos89@gmail.com>
2022-06-15 09:18:47 +0200

    Day three part 2 of 2015

--- src/day_03.rs
+++ src/day_03.rs
@@ -27,12 +27,29 @@ fn part_one() -> i32 {
 }
 
 fn part_two() -> i32 {
-    BufReader::new(File::open("./input/day_03.txt").unwrap());
+    let mut is_robo = false;
+    let mut current_position = [0, 0];
+    let mut robo_current_position = [0, 0];
+    let mut positions = std::collections::HashSet::new();
 
-    1
+    positions.insert(current_position);
+    BufReader::new(File::open("./input/day_03.txt").unwrap())
+        .bytes()
+        .for_each(|x| {
+            if is_robo {
+                robo_current_position = get_position(robo_current_position, x.unwrap() as char);
+                positions.insert(robo_current_position);
+            } else {
+                current_position = get_position(current_position, x.unwrap() as char);
+                positions.insert(current_position);
+            }
+            is_robo = !is_robo;
+        });
+
+    positions.len() as i32
 }
 
 pub fn show() {
     println!("- Part 1: {}", part_one());
-    //println!("- Part 2: {}", part_two());
+    println!("- Part 2: {}", part_two());
 }
